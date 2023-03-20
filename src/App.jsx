1e44d33ce315b848ab49e0fbc0f533fd521d59d2
Samee Vohra <samee.vohra@gmail.com>
2023-03-20 13:24:26 -0700

    updated eslint config and app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import Title from './components/Title.jsx'
+import { useState, useEffect } from 'react';
+
+import Title from './components/Title.jsx';
 
 const App = () => {
+  const [quiz, setQuiz] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/quiz');
+      const data = await response.json();
+      setQuiz(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <Title />
     </div>
   );
-}
+};
 
 export default App;
