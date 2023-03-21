780f23a709e3a85b6f22131dda817c6701fa0bdc
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 12:33:06 -0700

    added handleclick functionality for answer selection

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,16 +1,44 @@
-const QuestionBlock = ({ question }) => {
-  console.log(question);
+const QuestionBlock = ({
+  question,
+  chosenAnswerItems,
+  setChosenAnswerItems,
+  unansweredQuestionIds,
+  setUnansweredQuestionIds,
+  quizItemId,
+}) => {
+  const handleClick = () => {
+    setChosenAnswerItems((prevState) => {
+      return [...prevState, question.text];
+    });
+
+    setUnansweredQuestionIds(
+      unansweredQuestionIds.filter((id) => id !== quizItemId)
+    );
+  };
+
+  const disabledPick =
+    !chosenAnswerItems?.includes(question.text) &&
+    !unansweredQuestionIds?.includes(quizItemId);
+
   return (
-    <button className='question-block'>
+    <button
+      className='question-block'
+      onClick={handleClick}
+      disabled={disabledPick}
+    >
       <img src={question.image} alt={question.alt} />
 
       <h3>{question.text}</h3>
 
       <p className='photo-credit'>
         <span>Photo Credit: </span> <br />
-        <a href={question.image} rel='noreferrer' target='_blank' >{question.credit}</a>
+        <a href={question.image} rel='noreferrer' target='_blank'>
+          {question.credit}
+        </a>
         <span> via </span>
-        <a href='https://www.unsplash.com' rel='noreferrer' target='_blank'>Unsplash</a>
+        <a href='https://www.unsplash.com' rel='noreferrer' target='_blank'>
+          Unsplash
+        </a>
       </p>
     </button>
   );
