c8eac9fda254e0ba91b4f349a84cd6fad89d74bb
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 12:34:03 -0700

    refactored to pass props from app to questionblock

--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -1,7 +1,12 @@
 import QuestionBlock from './QuestionBlock';
 
-const QuestionsBlock = ({ quizItem }) => {
-  console.log({ quizItem });
+const QuestionsBlock = ({
+  quizItem,
+  chosenAnswerItems,
+  setChosenAnswerItems,
+  unansweredQuestionIds,
+  setUnansweredQuestionIds
+}) => {
   return (
     <>
       <h2 id={quizItem.id} className='question-title'>
@@ -9,7 +14,15 @@ const QuestionsBlock = ({ quizItem }) => {
       </h2>
       <div className='questions-container'>
         {quizItem.questions.map((question, _index) => (
-          <QuestionBlock key={_index} question={question} />
+          <QuestionBlock
+            key={_index}
+            quizItemId={quizItem.id}
+            question={question}
+            chosenAnswerItems={chosenAnswerItems}
+            setChosenAnswerItems={setChosenAnswerItems}
+            unansweredQuestionIds={unansweredQuestionIds}
+            setUnansweredQuestionIds={setUnansweredQuestionIds}
+          />
         ))}
       </div>
     </>
