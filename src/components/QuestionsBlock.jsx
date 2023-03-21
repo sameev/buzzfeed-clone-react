6563188fb8f91ae4799639900bf2369ac6c64114
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 11:09:18 -0700

    updated quesstionsblock component

--- src/components/QuestionsBlock.jsx
+++ src/components/QuestionsBlock.jsx
@@ -1,7 +1,19 @@
-const QuestionsBlock = () => {
-  return (
-    <div>QuestionsBlock</div>
-  )
-}
+import QuestionBlock from './QuestionBlock';
 
-export default QuestionsBlock
+const QuestionsBlock = ({ quizItem }) => {
+  console.log({ quizItem });
+  return (
+    <>
+      <h2 id={quizItem.id} className='question-title'>
+        {quizItem.text}
+      </h2>
+      <div className='questions-container'>
+        {quizItem.questions.map((question, _index) => (
+          <QuestionBlock key={_index} question={question} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default QuestionsBlock;
