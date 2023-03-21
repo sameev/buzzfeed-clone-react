c44107ff482f39fdfc0b9c327cd2376deedf679a
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 12:32:16 -0700

    added state and passed it down for unanswered questions and chosen answers

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ import QuestionsBlock from './components/QuestionsBlock.jsx';
 
 const App = () => {
   const [quiz, setQuiz] = useState(false);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const [chosenAnswerItems, setChosenAnswerItems] = useState([]);
+  const [unansweredQuestionIds, setUnansweredQuestionIds] = useState([]);
 
   const fetchData = async () => {
     try {
@@ -21,15 +19,32 @@ const App = () => {
     }
   };
 
-  console.log(quiz);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const unansweredIds = quiz?.content?.map(({ id }) => id);
+    setUnansweredQuestionIds(unansweredIds);
+  }, [quiz]);
+
+  console.log(chosenAnswerItems);
+  console.log(unansweredQuestionIds)
 
   return (
     <div className='app'>
       <Title title={quiz?.title} subtitle={quiz?.subtitle} />
       {quiz &&
-        quiz?.content.map((contentItem) => {
-          return <QuestionsBlock key={contentItem.id} quizItem={contentItem}/>
-        })}
+        quiz?.content.map((contentItem) => (
+          <QuestionsBlock
+            key={contentItem.id}
+            quizItem={contentItem}
+            chosenAnswerItems={chosenAnswerItems}
+            setChosenAnswerItems={setChosenAnswerItems}
+            unansweredQuestionIds={unansweredQuestionIds}
+            setUnansweredQuestionIds={setUnansweredQuestionIds}
+          />
+        ))}
     </div>
   );
 };
