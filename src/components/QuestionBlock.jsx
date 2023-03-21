3543e3975aaa56238b4c8f47fa2422f673a58761
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 11:10:29 -0700

    updated question block to render button with image, text, and photo credit

--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -1,7 +1,19 @@
-const QuestionBlock = () => {
+const QuestionBlock = ({ question }) => {
+  console.log(question);
   return (
-    <div>QuestionBlock</div>
-  )
-}
+    <button className='question-block'>
+      <img src={question.image} alt={question.alt} />
 
-export default QuestionBlock
+      <h3>{question.text}</h3>
+
+      <p className='photo-credit'>
+        <span>Photo Credit: </span> <br />
+        <a href={question.image} rel='noreferrer' target='_blank' >{question.credit}</a>
+        <span> via </span>
+        <a href='https://www.unsplash.com' rel='noreferrer' target='_blank'>Unsplash</a>
+      </p>
+    </button>
+  );
+};
+
+export default QuestionBlock;
