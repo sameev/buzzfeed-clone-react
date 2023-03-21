b3e957a14f42a472eff6b87669d2790153a615e1
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 10:25:25 -0700

    updated app component for questionsblock component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import Title from './components/Title.jsx';
+import QuestionsBlock from './components/QuestionsBlock.jsx';
 
 const App = () => {
   const [quiz, setQuiz] = useState(false);
@@ -13,15 +14,22 @@ const App = () => {
     try {
       const response = await fetch('http://localhost:8000/quiz');
       const data = await response.json();
+      console.log(data);
       setQuiz(data);
     } catch (err) {
       console.log(err);
     }
   };
 
+  console.log(quiz);
+
   return (
-    <div className="app">
-      <Title title={quiz.title} subtitle={quiz.subtitle}/>
+    <div className='app'>
+      <Title title={quiz?.title} subtitle={quiz?.subtitle} />
+      {quiz &&
+        quiz?.content.map((contentItem) => {
+          return <QuestionsBlock key={contentItem.id} quizItem={contentItem}/>
+        })}
     </div>
   );
 };
