f6a7f66c7aa8387546801ab470d285d25c455663
Samee Vohra <samee.vohra@gmail.com>
2023-03-21 17:32:49 -0700

    updated logic for answerblock component

--- src/components/AnswerBlock.jsx
+++ src/components/AnswerBlock.jsx
@@ -1,7 +1,31 @@
-const AnswerBlock = () => {
-  return (
-    <div>AnswerBlock</div>
-  )
-}
+import { useEffect, useState } from 'react';
 
-export default AnswerBlock
+const AnswerBlock = ({ answerOptions, chosenAnswers }) => {
+  const [result, setResult] = useState(answerOptions[0]);
+
+  useEffect(() => {
+    answerOptions.forEach((answer) => {
+      if (
+        chosenAnswers.includes(answer.combination[0]) &&
+        chosenAnswers.includes(answer.combination[1]) &&
+        chosenAnswers.includes(answer.combination[2])
+      ) {
+        setResult(answer);
+      }
+    });
+  }, [result]);
+
+  console.log(result);
+  console.log(result.description);
+
+  return (
+    <div className='answer-container' id='answer-container'>
+      <h2 className='answer-title'>{result.text}</h2>
+      <div className='answer-block'>
+        <img src={result?.image} alt={result?.alt} />
+      </div>
+    </div>
+  );
+};
+
+export default AnswerBlock;
